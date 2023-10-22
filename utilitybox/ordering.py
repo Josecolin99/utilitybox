0c2c2d13e7af311946ed031d4ca616630dffc727
Jose Angel Colin Najera <josecolin99@gmail.com>
2023-10-22 16:36:40 -0600

    feat(Add Documentation)
    In this commit added the implentation and one bas implemen but funcionall for insertion sort

--- utilitybox/ordering.py
+++ utilitybox/ordering.py
@@ -85,6 +85,21 @@ def selection_sort(data: list, reverse=False):
     return data
   
 def inserion_sort(data:list, reverse=False):
+    """
+    The `insertion_sort` function sorts a given list of data in ascending order by
+    default, or in descending order if the `reverse` parameter is set to `True`.
+    
+    Args:
+      data (list): The "data" parameter is a list of elements that you want to sort
+    using the insertion sort algorithm.
+      reverse: The "reverse" parameter is a boolean value that determines whether
+    the sorting should be in ascending order (False) or descending order (True). By
+    default, it is set to False, which means the sorting will be in ascending
+    order. Defaults to False
+    
+    Returns:
+      the sorted list.
+    """
     comparison = operator.gt if reverse else operator.lt
     for index, dato in enumerate(data):
         reverse_index = index
@@ -94,6 +109,32 @@ def inserion_sort(data:list, reverse=False):
         data[reverse_index] = dato
     return data
 
+def inserion_sort_bad_implement(data:list):
+    """
+    The function `inserion_sort_bad_implement` is an implementation of the
+    insertion sort algorithm in Python, but it has some inefficient and unnecessary
+    steps.
+    
+    Args:
+      data (list): The parameter `data` is a list of elements that needs to be
+    sorted using the insertion sort algorithm.
+    """
+    analizated_start_index = 0
+    analizated_end_index = 0
+    for index, dato in enumerate(data):
+        print(f'Indice: {index}, dato: {dato}')
+        reverse_index = index
+        min_index = 0
+        required_move = False
+        while reverse_index > 0:
+            reverse_index -= 1
+            if dato < data[reverse_index]:
+                min_index = reverse_index
+                required_move = True
+        if required_move:
+            data.pop(index)
+            data.insert(min_index, dato)
+        analizated_end_index += 1
 
 if __name__ == '__main__':
     reverse = False
